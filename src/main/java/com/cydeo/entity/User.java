5d563eba7131d522d96c3f76a5040c3457fc7d52
gadiribrahimov <iqq87@yahoo.com>
2022-11-03 21:42:05 -0700

    ticketing project day 1 Role entity to UI

---
+++ src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,31 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String passWord;
+    private boolean enabled;
+    private String phone;
+
+    @ManyToOne
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+
+}
